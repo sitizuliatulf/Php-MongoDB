5a361ae944663e5dfb0e9bb1520cbb8db497976c
sitizuliatul <sitizuliatul@gmail.com>
2018-01-31 22:39:14 +0700

    register

--- application/models/auth_model.php
+++ application/models/auth_model.php
@@ -1,17 +1,17 @@
 <?php
 
 class Auth_model extends CI_Model {
-	public function __construct(){
-		parent:: __construct();
-	}
+  public function __construct(){
+    parent:: __construct();
+  }
 
-	public function get_user($where){
-		// array('username' => 'username', 'password' => 'password');
-		return $this->mongo_db->get_where($this->collection, $where);
-	}
+  public function get_user($where){
+    // array('username' => 'username', 'password' => 'password');
+    return $this->mongo_db->get_where($this->collection, $where);
+  }
 
-	public function insert_data($data) {
-		return $this->mongo_db->insert($this->collection, $data);
-	}
+  public function insert_data($data) {
+    return $this->mongo_db->insert($this->collection, $data);
+  }
 
 }
