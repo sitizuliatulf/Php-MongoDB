cb8c93e97f85fb48ea23dd6b61590c08dbd44911
Kondel <azharprabui@gmail.com>
2018-02-01 21:34:35 +0700

    test

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -1,15 +1,17 @@
 <?php
 
-class Auth_model extends CI_Model
-{
-	public function __construct(){
-		parent:: __construct();
-	}
+class Auth_model extends CI_Model {
+  public function __construct(){
+    parent:: __construct();
+  }
 
-	public function get_user($collections, $where){
-		// array('username' => 'username', 'password' => 'password');
-		return $this->mongo_db->get_where($collections, $where);
-	}
+  public function get_user($where){
+    // array('username' => 'username', 'password' => 'password');
+    return $this->mongo_db->get_where($this->collection, $where);
+  }
 
+  public function insert_data($data) {
+    return $this->mongo_db->insert($this->collection, $data);
+  }
 
 }
